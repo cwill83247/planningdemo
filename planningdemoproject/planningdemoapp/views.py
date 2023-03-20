6d735703bf40fe94a5fa875631585c30bd62d7bf
cwill83247 <breakthrumusic@hotmail.com>
2023-03-20 11:41:59 +0000

    Initial Commit -setup basics

---
+++ planningdemoproject/planningdemoapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import openai
+
+OPENAI_API_KEY='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'
+
+@csrf_exempt
+def chatbot(request):
+    if request.method == 'POST':
+        data = request.POST.dict()
+        question = data.get('question', '')
+        response = ask_question(question)
+        return JsonResponse({'answer': response})
+
+    return render(request, 'planningchatgpt.html')
+
+def ask_question(question):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=(f"I have a question: {question}\n"
+                "Answer:"),
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    answer = response.choices[0].text.strip()
+    return answer
